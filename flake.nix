07659a627a659a323c2b46f97eab0ff7fc52f6fb
mjoes <morten.slingsby@gmail.com>
2025-01-19 21:41:11 +0100

    update

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/24.11-beta";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }@inputs:
+  outputs = { self, nixpkgs, home-manager }@inputs:
   let 
     system = "aarch64-linux";
     pkgs = import nixpkgs {
@@ -16,14 +18,38 @@
       inherit system;
       specialArgs = inputs;
       modules = [
-        ./orb/configuration.nix
+        home-manager.nixosModules.home-manager
+        ./hardware/configuration.nix
         {
+          programs.starship.enable = true;
+          users.users.mj.isNormalUser = true;
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.mj = {
+            home.stateVersion = "24.11";
+            programs.fzf.enable = true;
+          };
+        }
+        {
+          nix.extraOptions = ''
+            experimental-features = nix-command flakes
+          '';
           environment.systemPackages = with pkgs; [
             neovim
             gh
             git
             ruff
-            fzf
+            lazygit
+            go
+            gopls
+            python3Packages.jedi-language-server
+            lua-language-server
+            nixd
+            ripgrep
+          ];
+          fonts.packages = with pkgs; [
+            (nerdfonts.override { fonts = [ "Hack" ];})
+            fira-code
           ];
         }
       ];
